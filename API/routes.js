ce7f7071e3cb06e6d4447d11beebd4ab169db2ed
subramanian.v <subramanian.v@caratlane.com>
2020-11-26 14:44:34 +0530

    App to Class

--- API/routes.js
+++
@@ -1,7 +0,0 @@
-import ProcessController from "./process/controller";
-
-const Routes = async (app) => {
-  app.post("/ajiranet/process", ProcessController);
-};
-
-export default Routes;
--- app.js
+++ app.js
@@ -4,36 +4,53 @@ import express from "express";
 import morgan from "morgan";
 import logger from "./utils/logger";
 import { APP_NAME, PORT } from "./lib/constants";
-import Routes from "./API/routes";
+import ProcessController from "./API/process/controller";
 import bodyParser from "./utils/bodyparser";
 
-const app = express();
-
-// Logs Request Info
-app.use(
-  morgan((tokens, req, res) => {
-    logger.info(
-      [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens["response-time"](req, res),
-        "ms",
-      ].join(" ")
+class App {
+  #app = null;
+  constructor(params) {
+    Object.keys(params).map((x) => (this[x] = params[x]));
+  }
+  initApp() {
+    this.app = express();
+    this.addMiddlewares();
+    this.addHealtCheck();
+    this.addRoutes()
+    this.app.listen(PORT, () => {
+      logger.info(
+        `${this.APP_NAME} app listening at http://localhost:${this.PORT}`
+      );
+    });
+  }
+  addMiddlewares() {
+    this.app.use(
+      morgan((tokens, req, res) => {
+        logger.info(
+          [
+            tokens.method(req, res),
+            tokens.url(req, res),
+            tokens.status(req, res),
+            tokens["response-time"](req, res),
+            "ms",
+          ].join(" ")
+        );
+      })
     );
-  })
-);
+    this.app.use(bodyParser);
+  }
+  addHealtCheck() {
+    this.app.get("/", (req, res) => {
+      res.status(200).json({ message: "I am Alive" });
+    });
+  }
+  addRoutes() {
+    this.app.post("/ajiranet/process", ProcessController);
+  }
+  getApp() {
+    this.initApp();
+    return this.app;
+  }
+}
 
-app.use(bodyParser);
-
-// Healtcheck End point
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "I am Alive" });
-});
-
-Routes(app);
-
-// App Listens
-app.listen(PORT, () => {
-  logger.info(`${APP_NAME} app listening at http://localhost:${PORT}`);
-});
+new App({ APP_NAME, PORT }).getApp();
